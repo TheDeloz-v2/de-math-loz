00a26b8e1d81de5707d624ff8e6cffd35e1bf76e
Diego-Lemus <deloz.lemus.09@gmail.com>
2025-09-10 16:41:18 -0600

    [fix]: fix test

--- src/math_functions.py
+++ src/math_functions.py
@@ -23,13 +23,11 @@ def factorial(n: int) -> int:
 
 
 def is_prime(n: int) -> bool:
-    """
     if n <= 1:
         return False
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
-    """
     return True
 
 
